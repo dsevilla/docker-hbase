9ae4827060f6a7e730dae640350c4bb994bf86aa
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2019-07-10 01:36:09 +0200

    Change the order of the thrift execution to start when hbase is initialized.

--- standalone/run.sh
+++ standalone/run.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-/opt/hbase-$HBASE_VERSION/bin/hbase-daemon.sh start thrift
 /opt/hbase-$HBASE_VERSION/bin/start-hbase.sh
+/opt/hbase-$HBASE_VERSION/bin/hbase-daemon.sh start thrift
 tail -f /opt/hbase-$HBASE_VERSION/logs/*
